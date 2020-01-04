4279ac083f7f8fd137d85f132c1b7f62e9cc1e60
bin386@163.com <1358722618sxr>
2020-01-04 12:53:02 +0800

    修改gitignore文件，允许提交/dist文件夹(用户github发布网站)，提交dist文件夹，修改Navbar，删除不需要的右侧下拉

---
+++ dist/precache-manifest.505f89f2353c76cd3ff9c80704781b3a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6d35b37b7a73bbefb38",
+    "url": "/vue-typescript-admin-template/css/404.67979819.css"
+  },
+  {
+    "revision": "a6d35b37b7a73bbefb38",
+    "url": "/vue-typescript-admin-template/js/404.194173d4.js"
+  },
+  {
+    "revision": "a4c907e4a3e59fcc5421",
+    "url": "/vue-typescript-admin-template/css/app.5ed2e96f.css"
+  },
+  {
+    "revision": "a4c907e4a3e59fcc5421",
+    "url": "/vue-typescript-admin-template/js/app.7719b243.js"
+  },
+  {
+    "revision": "297c8a9b4d81876c91db",
+    "url": "/vue-typescript-admin-template/css/chunk-vendors.ee57d822.css"
+  },
+  {
+    "revision": "297c8a9b4d81876c91db",
+    "url": "/vue-typescript-admin-template/js/chunk-vendors.63ff5cc7.js"
+  },
+  {
+    "revision": "61a37bee31118defdbc0",
+    "url": "/vue-typescript-admin-template/css/dashboard.f034a0c6.css"
+  },
+  {
+    "revision": "61a37bee31118defdbc0",
+    "url": "/vue-typescript-admin-template/js/dashboard.e0e60b80.js"
+  },
+  {
+    "revision": "0d07db6efa11d18093a7",
+    "url": "/vue-typescript-admin-template/css/form.f68a73f9.css"
+  },
+  {
+    "revision": "0d07db6efa11d18093a7",
+    "url": "/vue-typescript-admin-template/js/form.17ec13e9.js"
+  },
+  {
+    "revision": "4aedbf483a8ec47204cc",
+    "url": "/vue-typescript-admin-template/css/login.1dd29b7d.css"
+  },
+  {
+    "revision": "4aedbf483a8ec47204cc",
+    "url": "/vue-typescript-admin-template/js/login.d688cf9d.js"
+  },
+  {
+    "revision": "47df526a707dbfa0419d",
+    "url": "/vue-typescript-admin-template/js/menu1.0a5bd261.js"
+  },
+  {
+    "revision": "8d54dca8cc7408047145",
+    "url": "/vue-typescript-admin-template/js/menu1-1.b6e880ab.js"
+  },
+  {
+    "revision": "5b08b3604ed482cb4b99",
+    "url": "/vue-typescript-admin-template/js/menu1-2.4915e8d1.js"
+  },
+  {
+    "revision": "d4136c46411b8370c368",
+    "url": "/vue-typescript-admin-template/js/menu1-2-1.e3327f08.js"
+  },
+  {
+    "revision": "2f04a806718cdc5dfa37",
+    "url": "/vue-typescript-admin-template/js/menu1-2-2.a34ad1e6.js"
+  },
+  {
+    "revision": "9ff42be06db7ce6cc6b0",
+    "url": "/vue-typescript-admin-template/js/menu1-3.928aba9d.js"
+  },
+  {
+    "revision": "a113afced17f58ac9efa",
+    "url": "/vue-typescript-admin-template/js/menu2.c1c046f6.js"
+  },
+  {
+    "revision": "9465503c81cbc2b89ae4",
+    "url": "/vue-typescript-admin-template/js/table.cb5d985b.js"
+  },
+  {
+    "revision": "a10da117941e8065128c",
+    "url": "/vue-typescript-admin-template/js/tree.64395134.js"
+  },
+  {
+    "revision": "0f4bc32b0f52f7cfb7d19305a6517724",
+    "url": "/vue-typescript-admin-template/img/404-cloud.0f4bc32b.png"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/vue-typescript-admin-template/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/vue-typescript-admin-template/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "a57b6f31fa77c50f14d756711dea4158",
+    "url": "/vue-typescript-admin-template/img/404.a57b6f31.png"
+  },
+  {
+    "revision": "bb918b12ad591964692cbe6e91aad81e",
+    "url": "/vue-typescript-admin-template/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-typescript-admin-template/robots.txt"
+  }
+];
